31d7da2b49d2d2bb6c9ed38031ca98b309183bc0
arsenaultk4 <arsenaultk4@wit.edu>
2020-07-25 17:59:46 -0400

    Make assembler more versatile

--- Assembler.py
+++ Assembler.py
@@ -34,67 +34,55 @@ class Assembler:
     labels = {}
     
     def __init__(self, path):
+        self.path = path
         if (os.path.isfile('temp.txt')):
             os.remove('temp.txt')
-        with open(path, 'r') as IFile, open('temp.txt', 'a+') as tempFile, open('decimal.txt', 'w+') as D_OFile, open('binary.txt', 'w+') as B_OFile:
-            D_OFile.write('See ' + path + ' for source code\n')
-            B_OFile.write('See ' + path + ' for source code\n')
+        with open(path, 'r') as IFile, open('temp.txt', 'a+') as temp_file:
             # First pass: deal with labels (record location and remove), replace XZR with X31
-            lineNum = 0
+            line_num = 0
             for line in IFile:
                 if ('XZR' in line):
                     line = line.replace('XZR', 'X31')
                 if (':' in line):
-                    tempFile.write(line.split(':')[1].lstrip())
-                    self.labels[line.split(':')[0]] = lineNum
+                    temp_file.write(line.split(':')[1].lstrip())
+                    self.labels[line.split(':')[0]] = line_num
                 else:
-                    tempFile.write(line.lstrip())
-                lineNum = lineNum + 1
+                    temp_file.write(line.lstrip())
+                line_num = line_num + 1
 
-    def convertR(self, opcode, data):
+    def __get_decimal(self, opcode, data, line_num, type):
         opc = self.OP_CONVERSION[opcode][0]
-        if (opcode == 'LSL' or opcode == 'LSR'):
-            rm = '0'
-            shamt = data.split()[2][1:]
+        if (type == 'R'):
+            if (opcode == 'LSL' or opcode == 'LSR'):
+                rm = '0'
+                shamt = data.split()[2][1:]
+            else:
+                rm = data.split()[2][1:]
+                shamt = '0'
+            rn = data.split()[1][1:-1]
+            rd = data.split()[0][1:-1]
+            decimal_form = opc + ' ' + rm + ' ' + shamt + ' ' + rn + ' ' + rd
+        elif (type == 'I'):
+            immediate = data.split()[2][1:]
+            rn = data.split()[1][1:-1]
+            rd = data.split()[0][1:-1]
+            decimal_form = opc + ' ' + immediate + ' ' + rn + ' ' + rd
+        elif (type == 'D'):
+            address = data.split()[2][1:-1]
+            op2 = '0'
+            rn = data.split()[1][2:-1]
+            rt = data.split()[0][1:-1]
+            decimal_form = opc + ' ' + address + ' ' + op2 + ' ' + rn + ' ' + rt 
+        elif (type == 'CB'):
+            address = str(self.labels[data] - line_num)
+            rt = self.OP_CONVERSION[opcode][2]
+            decimal_form = opc + ' ' + address + ' ' + rt
         else:
-            rm = data.split()[2][1:]
-            shamt = '0'
-        rn = data.split()[1][1:-1]
-        rd = data.split()[0][1:-1]
-        decimalForm = opc + ' ' + rm + ' ' + shamt + ' ' + rn + ' ' + rd
-        return decimalForm
+            address = str(self.labels[data] - line_num)
+            decimal_form = opc + ' ' + address
+        return decimal_form
 
-    def convertI(self, opcode, data):
-        opc = self.OP_CONVERSION[opcode][0]
-        immediate = data.split()[2][1:]
-        rn = data.split()[1][1:-1]
-        rd = data.split()[0][1:-1]
-        decimalForm = opc + ' ' + immediate + ' ' + rn + ' ' + rd
-        return decimalForm
-
-    def convertD(self, opcode, data):
-        opc = self.OP_CONVERSION[opcode][0]
-        address = data.split()[2][1:-1]
-        op2 = '0'
-        rn = data.split()[1][2:-1]
-        rt = data.split()[0][1:-1]
-        decimalForm = opc + ' ' + address + ' ' + op2 + ' ' + rn + ' ' + rt 
-        return decimalForm
-
-    def convertCB(self, opcode, data, lineNum):
-        opc = self.OP_CONVERSION[opcode][0]
-        address = str(self.labels[data] - lineNum)
-        rt = self.OP_CONVERSION[opcode][2]
-        decimalForm = opc + ' ' + address + ' ' + rt
-        return decimalForm
-
-    def convertB(self, opcode, data, lineNum):
-        opc = self.OP_CONVERSION[opcode][0]
-        address = str(self.labels[data] - lineNum)
-        decimalForm = opc + ' ' + address
-        return decimalForm
-
-    def convertBinary(self, decimal, size):
+    def __convert_binary(self, decimal, size):
         decimal = int(decimal)
         binary = str(bin(decimal)).split('b')[1]
         binary = binary.zfill(size)
@@ -103,81 +91,128 @@ class Assembler:
                 binary = str(bin(int(binary, 2) - (1 << size)).split('b')[1])
         return binary
 
-    def getBinary(self, decimal, type):
+    def __getBinary(self, decimal, type):
         decimal = decimal.split()
         print(decimal)
         if (type == 'R'):
-            opc = self.convertBinary(decimal[0], 11)
-            rm = self.convertBinary(decimal[1], 5)
-            shamt = self.convertBinary(decimal[2], 6)
-            rn = self.convertBinary(decimal[3], 5)
-            rd = self.convertBinary(decimal[4], 5)
-            binaryForm = opc + ' ' + rm + ' ' + shamt + ' ' + rn + ' ' + rd
+            opc = self.__convert_binary(decimal[0], 11)
+            rm = self.__convert_binary(decimal[1], 5)
+            shamt = self.__convert_binary(decimal[2], 6)
+            rn = self.__convert_binary(decimal[3], 5)
+            rd = self.__convert_binary(decimal[4], 5)
+            binary_form = opc + ' ' + rm + ' ' + shamt + ' ' + rn + ' ' + rd
         elif (type == 'I'):
-            opc = self.convertBinary(decimal[0], 10)
-            immediate = self.convertBinary(decimal[1], 12)
-            rn = self.convertBinary(decimal[2], 5)
-            rd = self.convertBinary(decimal[3], 5)
-            binaryForm = opc + ' ' + immediate + ' ' + rn + ' ' + rd
+            opc = self.__convert_binary(decimal[0], 10)
+            immediate = self.__convert_binary(decimal[1], 12)
+            rn = self.__convert_binary(decimal[2], 5)
+            rd = self.__convert_binary(decimal[3], 5)
+            binary_form = opc + ' ' + immediate + ' ' + rn + ' ' + rd
         elif (type == 'D'):
-            opc = self.convertBinary(decimal[0], 11)
-            address = self.convertBinary(decimal[1], 9)
-            op2 = self.convertBinary(decimal[2], 2)
-            rn = self.convertBinary(decimal[3], 5)
-            rt = self.convertBinary(decimal[4], 5)
-            binaryForm = opc + ' ' + address + ' ' + op2 + ' ' + rn + ' ' + rt 
+            opc = self.__convert_binary(decimal[0], 11)
+            address = self.__convert_binary(decimal[1], 9)
+            op2 = self.__convert_binary(decimal[2], 2)
+            rn = self.__convert_binary(decimal[3], 5)
+            rt = self.__convert_binary(decimal[4], 5)
+            binary_form = opc + ' ' + address + ' ' + op2 + ' ' + rn + ' ' + rt 
         elif (type == 'CB'):
-            opc = self.convertBinary(decimal[0], 8)
-            address = self.convertBinary(decimal[1], 19)
-            rt = self.convertBinary(decimal[2], 5)
-            binaryForm = opc + ' ' + address + ' ' + rt
+            opc = self.__convert_binary(decimal[0], 8)
+            address = self.__convert_binary(decimal[1], 19)
+            rt = self.__convert_binary(decimal[2], 5)
+            binary_form = opc + ' ' + address + ' ' + rt
         else:
-            opc = self.convertBinary(decimal[0], 6)
-            address = self.convertBinary(decimal[1], 26)
-            binaryForm = opc + ' ' + address
-        return binaryForm
+            opc = self.__convert_binary(decimal[0], 6)
+            address = self.__convert_binary(decimal[1], 26)
+            binary_form = opc + ' ' + address
+        return binary_form
 
-    def convert(self):
-        with open('temp.txt', 'r') as tempFile, open('decimal.txt', 'a+') as D_OFile, open('binary.txt', 'a+') as B_OFile:
+    def to_files(self):
+        with open('temp.txt', 'r') as temp_file, open('decimal.txt', 'a+') as D_OFile, open('binary.txt', 'a+') as B_OFile:
+            D_OFile.write('See ' + self.path + ' for source code\n')
+            B_OFile.write('See ' + self.path + ' for source code\n')
             # Second pass: convert
-            lineNum = 0
-            for line in tempFile:
+            line_num = 0
+            for line in temp_file:
                 opcode = line.split(' ', 1)[0]
                 data = line.split(' ', 1)[1].rstrip()
                 if (opcode in self.OP_CONVERSION):
                     opcodeFormat = self.OP_CONVERSION[opcode][1]
                     if (opcodeFormat == 'R'):
-                        d_line = self.convertR(opcode, data)
-                        b_line = self.getBinary(d_line, 'R')
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
                     elif (opcodeFormat == 'I'):
-                        d_line = self.convertI(opcode, data)
-                        b_line = self.getBinary(d_line, 'I')
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
                     elif (opcodeFormat == 'D'):
-                        d_line = self.convertD(opcode, data)
-                        b_line = self.getBinary(d_line, 'D')
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
                     elif (opcodeFormat == 'CB'):
-                        d_line = self.convertCB(opcode, data, lineNum)
-                        b_line = self.getBinary(d_line, 'CB')
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
                     else:
-                        d_line = self.convertB(opcode, data, lineNum)
-                        b_line = self.getBinary(d_line, 'B')
-                    D_OFile.write(d_line + '\n')
-                    B_OFile.write(b_line + '\n')
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
+                    D_OFile.write(d_line)
+                    B_OFile.write(b_line)
                 else:
                     D_OFile.write('Error: ' + opcode + ' not found')
                     B_OFile.write('Error: ' + opcode + ' not found')
-                lineNum = lineNum + 1
+                line_num = line_num + 1
 
-def main():
-    print('Enter file name for LEGv8 code: ')
-    path = input()
+    def decimal_to_list(self):
+        decimal_list = []
+        with open('temp.txt', 'r') as temp_file:
+            # Second pass: convert
+            line_num = 0
+            for line in temp_file:
+                opcode = line.split(' ', 1)[0]
+                data = line.split(' ', 1)[1].rstrip()
+                if (opcode in self.OP_CONVERSION):
+                    opcodeFormat = self.OP_CONVERSION[opcode][1]
+                    if (opcodeFormat == 'R'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                    elif (opcodeFormat == 'I'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                    elif (opcodeFormat == 'D'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                    elif (opcodeFormat == 'CB'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                    else:
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                    decimal_list.append(d_line)
+                else:
+                    decimal_list.append('Error: ' + opcode + ' not found')
+                line_num = line_num + 1
+        self.decimal_list = decimal_list
+        return decimal_list
 
-    if (os.path.isfile(path)):
-        LEGv8_Assembler = Assembler(path)
-        LEGv8_Assembler.convert()
-    else:
-        print('Error: Invalid path')
-
-
-if __name__ == '__main__':
-    main()
+    def binary_to_list(self):
+        binary_list = []
+        with open('temp.txt', 'r') as temp_file:
+            # Second pass: convert
+            line_num = 0
+            for line in temp_file:
+                opcode = line.split(' ', 1)[0]
+                data = line.split(' ', 1)[1].rstrip()
+                if (opcode in self.OP_CONVERSION):
+                    opcodeFormat = self.OP_CONVERSION[opcode][1]
+                    if (opcodeFormat == 'R'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
+                    elif (opcodeFormat == 'I'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
+                    elif (opcodeFormat == 'D'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
+                    elif (opcodeFormat == 'CB'):
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
+                    else:
+                        d_line = self.__get_decimal(opcode, data, line_num, opcodeFormat)
+                        b_line = self.__getBinary(d_line, opcodeFormat)
+                    binary_list.append(b_line)
+                else:
+                    binary_list.append('Error: ' + opcode + ' not found')
+                line_num = line_num + 1
+        self.binary_list = binary_list
+        return binary_list
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # processor
+
+The first part of this is the assembler. The assembler will take ARMv8 code and convert it to decimal and binary.
+
+This is the comprehensive list of functions that can be processed:
+- ADD
+- SUB
+- AND
+- ORR
+- EOR
+- LSL
+- LSR
+- ADDI
+- SUBI
+- ANDI
+- ORRI
+- EORI
+- SUBS
+- SUBIS
+- LDUR
+- STUR
+- CBZ
+- CBNZ
+- B
+- B.EQ
+- B.NE
+- B.GT
+- B.GTE
+- B.LT
+- B.LTE
